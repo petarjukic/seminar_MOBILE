6da835600f47d6760621d07033f91d2b7a263ff7
petarjukic <jukicpetar2@gmail.com>
2022-01-22 22:12:26 +0100

    recipe list added

--- Recepti/app/src/main/java/com/example/recepti/RecipeListActivity.java
+++ Recepti/app/src/main/java/com/example/recepti/RecipeListActivity.java
@@ -28,33 +28,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-/*
-TO GET DATA FROM ANOTHER TABLE ACCORDING TO A RETRIEVED DATA IN FIREBASE
-DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-databaseReference= rootRef.child("appointment");
-databaseReference.orderByChild("userid").equalTo(userid1).addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
-        for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) {
-            String theraid = dataSnapshot1.child("theraid").getValue(String.class);
-            DatabaseReference userRef = rootRef.child("alluser/thera").child(theraid);
-            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot userSnapshot) {
-                    String name = userSnapshot.child("name").getValue(String.class);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    throw databaseError.toException();
-                }
-            }
-        }
-       adapter=new MyRecyclerviewPAppointment(MainActivityPAppointment.this, a,namelist);
-        rv.setAdapter(adapter);
-
-    }
- */
 
 public class RecipeListActivity extends AppCompatActivity {
 
@@ -91,7 +64,7 @@ public class RecipeListActivity extends AppCompatActivity {
         recipeAdapter = new MyViewAdapter(this, recipeList);
         recViewRecipe.setAdapter(recipeAdapter);
 
-        if(currentUser == null) {
+        if (currentUser == null) {
             loginButton.setVisibility(View.VISIBLE);
             loginButton.setOnClickListener(v -> {
                 Intent intent = new Intent(RecipeListActivity.this, LoginUserActivity.class);
@@ -99,8 +72,7 @@ public class RecipeListActivity extends AppCompatActivity {
                 loginButton.setVisibility(View.GONE);
             });
             Toast.makeText(RecipeListActivity.this, "You are not logged in", Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             loginButton.setVisibility(View.GONE);
             logoutButton.setVisibility(View.VISIBLE);
 
@@ -116,47 +88,28 @@ public class RecipeListActivity extends AppCompatActivity {
             startActivity(intent);
         });
         String category = "";
-        if(getIntent().hasExtra("category")) {
+        if (getIntent().hasExtra("category")) {
             category = getIntent().getStringExtra("category");
             category = category.toLowerCase();
             //Toast.makeText(getApplicationContext(), category + " ovo je u kategoriji", Toast.LENGTH_SHORT).show();
         }
-        Query query = dbRef.orderByChild("category").equalTo(category);
 
-        Query query1 = FirebaseDatabase.getInstance().getReference("recipe")
+        Query query = FirebaseDatabase.getInstance().getReference("recipe")
                 .orderByChild("category")
                 .equalTo(category);
 
-        query1.addListenerForSingleValueEvent(valueEventListener);  //DOLI
+        query.addListenerForSingleValueEvent(valueEventListener);
 
-
-
-        /*query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Toast.makeText(RecipeListActivity.this, snapshot.toString(), Toast.LENGTH_LONG).show();
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-                    String categorija = dataSnapshot.child("category").getValue(String.class);
-                    //Log.d("categorija", "Ovo je kategorija koja sa odabra za recept " + categorija);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });*/
     }
+
     ValueEventListener valueEventListener = new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull DataSnapshot snapshot) {
             for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                //Artist artist = snapshot.getValue(Artist.class);
                 Recipe recipe = dataSnapshot.getValue(Recipe.class);
-                Log.d("category", "Recept ime " + recipe.getTitle());
-                //Log.d("category", "AA" + dataSnapshot.getValue(Recipe.class).toString());
-                //artistList.add(artist);
+                recipeList.add(recipe);
             }
+            recipeAdapter.notifyDataSetChanged();
         }
 
         @Override
--- Recepti/app/src/main/java/com/example/recepti/adapter/MyViewAdapter.java
+++ Recepti/app/src/main/java/com/example/recepti/adapter/MyViewAdapter.java
@@ -12,12 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recepti.R;
 import com.example.recepti.models.Recipe;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class MyViewAdapter extends RecyclerView.Adapter<MyViewAdapter.MyViewHolder> {
-    private List<Recipe> recipeList;
-    private Context context;
+    private final List<Recipe> recipeList;
+    private final Context context;
 
     public MyViewAdapter(Context context, List<Recipe> recipeList) {
         this.recipeList = recipeList;
@@ -40,13 +41,15 @@ public class MyViewAdapter extends RecyclerView.Adapter<MyViewAdapter.MyViewHold
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.getTitle().setText(recipeList.get(position).getTitle());
         holder.getDescription().setText(recipeList.get(position).getDescription());
+        Picasso.get().load(recipeList.get(position).getImage()).into(holder.getImage());
         //ZA SLIKU
         //Picasso.get().load(items.get(position).owner.getAvatar_url()).into(holder.getImageView());
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
-        private TextView  title, description;
-        private ImageView image;
+        private final TextView title;
+        private final TextView description;
+        private final ImageView image;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
